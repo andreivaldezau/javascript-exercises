b06533ad931e4451dcba10bf408864ec848e5445
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-06-15 00:36:44 +1000

    Add attempt for 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+// Use arithmetic progression formula
+const sumAll = function (a, b) {
+  if (a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    return ((Math.abs(a - b) + 1) * (a + b)) / 2;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
