c2e3f33d40776b75483a8c2b81133c3d5ed2e2c3
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-06-27 09:04:54 +1000

    Add attempt for 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  const filtered = [...input.toLowerCase()]
+    .filter((character) => /[a-zA-Z0-9]/.test(character))
+    .join("");
+  const reversed = [...filtered].reverse().join("");
+  return filtered === reversed;
 };
 
 // Do not edit below this line
