7f8f880f11444abdc410f3190785801fabf17c0e
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-06-27 10:07:01 +1000

    Add attempt for 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  return people.sort(
+    (a, b) =>
+      (b.yearOfDeath || currentYear) -
+      b.yearOfBirth -
+      ((a.yearOfDeath || currentYear) - a.yearOfBirth)
+  )[0];
 };
 
 // Do not edit below this line
